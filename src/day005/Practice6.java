a30768af9f3d21870e7e3c862e0b6e1cbc41bfa0
leehyeon2021 <leehyeon2021@gmail.com>
2026-01-26 16:31:41 +0900

    260126 오후

--- src/day005/Practice6.java
+++ src/day005/Practice6.java
@@ -10,7 +10,7 @@ public class Practice6 {
         /*[문제 1] 정수 5개(10, 20, 30, 40, 50)를 저장할 수 있는 int 타입의 배열 numbers1를 선언과 동시에 초기화하세요.
         배열의 2번 인덱스에 저장된 값을 콘솔에 출력하세요.*/
         // int[] numbers1 = new int[5];  --> 배열 '선언'만 한 거.
-        int[] numbers1 = { 10, 20, 30, 40, 50}; // --> 배열 '선언' , '초기화'
+        int[] numbers1 = {10, 20, 30, 40, 50}; // --> 배열 '선언' , '초기화'
         System.out.println(numbers1[2]);
 
         /*[문제 2] String 타입의 데이터를 3개 저장할 수 있는 배열 season을 new 키워드를 사용하여 생성하세요.
@@ -25,7 +25,7 @@ public class Practice6 {
         /*[문제 3] 다음과 같은 과일 배열이 있습니다. for 반복문을 사용하여 배열의 모든 요소를 하나씩 콘솔에 출력하시오.
         선언 코드:*/
         String[] fruits = {"사과", "바나나", "포도", "딸기"};
-        for(int i = 0; i <= fruits.length-1 ; i++){
+        for (int i = 0; i <= fruits.length - 1; i++) {
             System.out.println(fruits[i]);
         }
 
@@ -34,50 +34,55 @@ public class Practice6 {
         선언 코드:*/
         int[] scores1 = {85, 92, 78, 65, 95};
         int sum4 = 0;
-        for(int index =0 ; index<= scores1.length-1 ; index++ ){
+        for (int index = 0; index <= scores1.length - 1; index++) {
             sum4 += scores1[index];
         }
-        System.out.printf("합계: %d , 평균: %f\n", sum4 , sum4 / (double)scores1.length);
+        System.out.printf("합계: %d , 평균: %f\n", sum4, sum4 / (double) scores1.length);
 
 
         /*[문제 5] 점수 배열에서 100점 만점자가 처음 나타나면, "100점 만점자를 찾았습니다!"라고 출력하고 반복문을 즉시 종료하는 프로그램을 작성하시오.
-        선언 코드:*/ int[] scores2 = {77, 82, 100, 54, 96};
-        for(int i = 0 ; i <= scores2.length-1 ; i++ ){
-            if(scores2[i] == 100){
+        선언 코드:*/
+        int[] scores2 = {77, 82, 100, 54, 96};
+        for (int i = 0; i <= scores2.length - 1; i++) {
+            if (scores2[i] == 100) {
                 System.out.println("100점 만점자를 찾았습니다!");
                 break;
             }
         }
         // 향상된 for문으로 풀기
-        for( int i : scores2){
-            if(i == 100){
+        for (int i : scores2) {
+            if (i == 100) {
                 System.out.println("100점 만점자가 나타났다");
             }
         }
 
         /*[문제 6] 다음 배열에서 'A'형 혈액형을 가진 사람이 몇 명인지 for 반복문을 통해 세고, 그 수를 콘솔에 출력하시오.
-        선언 코드:*/ String[] bloodTypes = {"A", "B", "O", "AB", "A", "B", "A"};
+        선언 코드:*/
+        String[] bloodTypes = {"A", "B", "O", "AB", "A", "B", "A"};
         int A = 0;
-        for(int i = 0; i<= bloodTypes.length-1 ; i++){
-            if(bloodTypes[i].equals("A")) {
+        for (int i = 0; i <= bloodTypes.length - 1; i++) {
+            if (bloodTypes[i].equals("A")) {
                 A += 1;
             }
         }
-        System.out.println(A+"명");
+        System.out.println(A + "명");
         // 향상된 for문
         int count = 0;
-        for( String blood : bloodTypes){
-            if(blood.equals("A")){
+        for (String blood : bloodTypes) {
+            if (blood.equals("A")) {
                 count++;
             }
         }
         System.out.println("A형: " + count + "명");
 
         /*[문제 7] 주어진 숫자 배열에서 가장 큰 값을 찾아 콘솔에 출력하는 프로그램을 작성하시오.
-        선언 코드:*/ int[] numbers2 = {23, 5, 67, 12, 88, 34};
+        선언 코드:*/
+        int[] numbers2 = {23, 5, 67, 12, 88, 34};
         int max7 = 0;
-        for(int value : numbers2){
-            if(max7 < value){ max7 = value;}
+        for (int value : numbers2) {
+            if (max7 < value) {
+                max7 = value;
+            }
         }
         System.out.println(max7);
 
@@ -88,24 +93,33 @@ public class Practice6 {
         String[] products = {"볼펜", "노트", "지우개"};
         int[] stock = {10, 5, 20};
         Scanner scan = new Scanner(System.in);
-        System.out.print("상품명: ");  String products8 =  scan.next();
-        System.out.print("수량: ");   int stock8 = scan.nextInt();
+        System.out.print("상품명: ");
+        String products8 = scan.next();
+        System.out.print("수량: ");
+        int stock8 = scan.nextInt();
         int check = 0;  // 0이면 제품 없다는 뜻을 임의로 선정.
-        for(int index = 0; index <= products.length-1 ; index++ ){
-            if(products[index].equals(products8)){  // 만약에 index번째 제품명과 입력받은 제품명이 같으면
-                if(stock[index] >= stock8) {         // 만약에 index번째 재고가 입력받은 재고보다 이상이면
+        for (int index = 0; index <= products.length - 1; index++) {
+            if (products[index].equals(products8)) {  // 만약에 index번째 제품명과 입력받은 제품명이 같으면
+                if (stock[index] >= stock8) {         // 만약에 index번째 재고가 입력받은 재고보다 이상이면
                     stock[index] -= stock8;         // 입력받은 재고 수만큼 차감
                     check = 1; // 1이면 '구매완료' 뜻을 정해뒀다.
                     break;
-                }else{
-                    check = 2; // 2이면 '제품 없음'을 뜻한다고 정해뒀다.
-                    //System.out.println("제품이 존재하지 않습니다."); -> 여기서 떠버리면 계속 뜸
-                } // if, else end
-            } // if end
+                }
+            }else {
+                check = 2; // 2이면 '제품 없음'을 뜻한다고 정해뒀다.
+                //System.out.println("제품이 존재하지 않습니다."); -> 여기서 떠버리면 계속 뜸
+            }
         } // for end
-        if(check==2){System.out.println("제품이 존재하지 않습니다.");}   // for 밖에서 (끝나고) 제품없음 출력한다.
-        else if(check ==1){System.out.println("구매완료");}           // 출력은 그냥 반복문 끝나고 하는 게 나을 듯
-        else if(check==0){System.out.println("재고 부족");}
+        if (check == 2) {
+            System.out.println("제품이 존재하지 않습니다.");
+        }   // for 밖에서 (끝나고) 제품없음 출력한다.
+        else if (check == 1) {
+            System.out.println("구매완료");
+        }           // 출력은 그냥 반복문 끝나고 하는 게 나을 듯
+        else if (check == 0) {
+            System.out.println("재고 부족");
+        }
+        System.out.print("\n");
 
         /*[문제 9] 주어진 영화 이름과 평점 배열을 이용하여, 각 영화의 평점을 별(★, ☆)로 시각화하여 출력하는 프로그램을 작성하시오.(* 영화명과 평점 인덱스가 같다는 가정 )
         요구 조건: 각 영화의 평점(10점 만점)만큼 꽉 찬 별(★)을, 나머지 점수만큼 빈 별(☆)을 출력합니다.
@@ -118,12 +132,28 @@ public class Practice6 {
         청설 ★★★★★★☆☆☆☆*/
         String[] movieNames = {"히든페이스", "위키드", "글래디에이터2", "청설"};
         int[] movieRatings = {8, 4, 7, 6};
+        for (int i = 0; i <= movieRatings.length - 1; i++){ // 여기 왜 향상된 for문으로 안 하셨는지 놓쳐서 못 들음!! 영상 보기
+            String movie = movieNames[i];
+            System.out.print(movie);
+            for(int star = 1 ; star <= 10; star++){
+                if(star <= movieRatings[i]){
+                    System.out.print("★");
+                }
+                else{
+                    System.out.print("☆");
+                }
+            }
+            System.out.println(); // i가 바뀔 때마다 줄바꿈
+        }
+        System.out.print("\n");
 
-        /*[문제 10] 차량별 주차 시간 데이터가 주어졌을 때, 아래의 요금 규정에 따라 각 차량이 지불해야 할 최종 주차 요금을 계산하여 출력하시오.(* 차량번호 와 이용시간 인덱스가 같다는 가정 )
+        /*[문제 10] 차량별 주차 시간 데이터가 주어졌을 때,
+        아래의 요금 규정에 따라 각 차량이 지불해야 할 최종 주차 요금을 계산하여 출력하시오.
+        (* 차량번호 와 이용시간 인덱스가 같다는 가정 )
         요금 규정:
-        기본 요금: 최초 30분까지 1,000원
-        추가 요금: 30분 초과 시, 매 10분마다 500원씩 추가
-        일일 최대 요금: 20,000원 (아무리 오래 주차해도 20,000원을 초과할 수 없음)
+            기본 요금: 최초 30분까지 1,000원
+            추가 요금: 30분 초과 시, 매 10분마다 500원씩 추가
+            일일 최대 요금: 20,000원 (아무리 오래 주차해도 20,000원을 초과할 수 없음)
         선언 코드:
         출력 예시:
         210어7125: 65분 주차, 최종 요금: 2500원
@@ -132,6 +162,33 @@ public class Practice6 {
         931나8234: 420분 주차, 최종 요금: 20000원*/
         String[] carNumbers = {"210어7125", "142가7415", "888호8888", "931나8234"};
         int[] usageMinutes = {65, 30, 140, 420};
+        // 1) money 요금 변수 선언
+        int money = 0;
+        for(int index = 0; index <= carNumbers.length - 1; index++){
+            String car = carNumbers[index]; // index번째 차량번호 추출
+            System.out.print( car );
+            int useTime = usageMinutes[index]; // index번째 사용시간 추출
+            if(useTime <= 30){ money = 1000; }
+            else{
+                money = ( useTime - 30 ) / 10;  // 사용시간 나누기 10분
+                money *= 5000; // 10분당 5000원
+                money += 1000; // 기본 요금 1000원;
+            }
+            // 만약에 금액이 2만원 이상이면 2만원. 아니면 금액 그대로.
+            money = money > 20000 ? 20000 : money;
+            System.out.println(useTime+"분 주차. 요금:"+money);
+        } // for end
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/종합/예제2/boardService2.java
@@ -0,0 +1,49 @@
+package 종합.예제2;
+
+import java.util.Scanner;
+
+public class boardService2 {
+    public static void main(String[] args) {
+        // [1] 메모리 설계: 데이터 최대 100개 저장
+            // 1) 2개 정보를 가진 게시물이 3개이면 변수는 총 6개 갖는다.
+            // 2) 2개 정보를 가진 게시물이 100개이면 변수는 총 200개 갖는다. --> 관리가 어렵기에 배열을 사용한다.
+        String[] contents = new String[100];    // 제목 100개 , 각 요소는 객체타입이므로 null 값을 갖는다.
+        String[] writers = new String[100];     // 작성자 100개 , { null , null , null ~~ }
+
+        // [2] 반복문 - 무한루프
+        for( ; ; ){
+            // 메뉴 보여주기
+            System.out.println("======= My community =======");
+            System.out.println("1. 게시물 쓰기  2. 게시물 출력");
+            System.out.println("============================");
+            System.out.print("선택> ");
+            // 입력 받기
+            Scanner scan = new Scanner(System.in);
+            int input = scan.nextInt();
+            if(input == 1){     // 제어문/코드의 흐름(경우의 수) 판단한다.
+                scan.nextLine();
+                System.out.print("내용: "); String content = scan.nextLine(); // 입력 받고
+                System.out.print("작성자: "); String writer = scan.nextLine();
+                boolean save = false; // 저장했다 true , 저장못했다 false;
+                for(int index = 0; index < contents.length; index++) {
+                    if (contents[index] == null && writers[index] == null) {  // index번째 요소값이 비어있으면
+                        contents[index] = content; writers[index] = writer; // 입력받은 값들을 index번째에 대입한다.
+                        save = true;
+                        break;
+                    }
+                }
+                if (save == true) { System.out.println("[안내] 글쓰기 성공"); }
+                else { System.out.println("[경고] 게시물을 등록할 공간이 부족합니다."); }
+
+            } else if (input ==2) {
+                    for( int i = 0; i < contents.length; i++){
+                        if(contents[i] != null && writers[i] != null) {
+                            System.out.printf("작성자: %s, 내용: %s \n" , contents[i], writers[i]);
+                        }
+                    }
+                }
+
+        }
+
+    } // main end
+}
